7ec6bfdc7d2db956e6f82845df1a0d82f236d956
Oscar Ossa <oskrlex17@gmail.com>
2024-03-30 08:36:35 -0500

    update buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,43 +3,34 @@ import { SocialIcon } from "react-social-icons";
 
 function App() {
   return (
-    <div className="min-h-screen p-5 grid place-items-center">
+    <div className="min-h-screen p-5 grid place-items-center font-[Poppins]">
       <div className="text-center">
         <img src={Logo} alt="logo" />
         <h1 className="text-[#7c2e82] text-3xl font-bold mb-10 mt-10">
           ¡Bienvenido!
         </h1>
         <div className="flex justify-center space-x-6 mt-5">
-          <button
-            onClick={() => console.log("click")}
-            className="rounded-full p-2 bg-white"
-          >
-            <SocialIcon
-              network="whatsapp"
-              bgColor="#25d366"
-              style={{ height: 50, width: 50 }}
-            />
-          </button>
-          <button
-            onClick={() => console.log("click")}
-            className="rounded-full p-2 bg-white"
-          >
-            <SocialIcon
-              network="facebook"
-              bgColor="#3b5998"
-              style={{ height: 50, width: 50 }}
-            />
-          </button>
-          <button
-            onClick={() => console.log("click")}
-            className="rounded-full p-2 bg-white"
-          >
-            <SocialIcon
-              network="instagram"
-              bgColor="#e4405f"
-              style={{ height: 50, width: 50 }}
-            />
-          </button>
+          <SocialIcon
+            network="whatsapp"
+            bgColor="#25d366"
+            style={{ height: 50, width: 50 }}
+            url="https://wa.me/+573185533874?text=Hola,%20estoy%20interesado%20en%20tu%20producto."
+            className="hover:opacity-80"
+          />
+          <SocialIcon
+            network="facebook"
+            bgColor="#3b5998"
+            className="hover:opacity-80"
+            style={{ height: 50, width: 50 }}
+            url="https://www.facebook.com/people/Red-Planet/100041521789739/?mibextid=WC7FNe&rdid=d1oa7HkyCr58fpX7"
+          />
+          <SocialIcon
+            network="instagram"
+            bgColor="#e4405f"
+            style={{ height: 50, width: 50 }}
+            url="https://www.instagram.com/internet_red_planet/"
+            className="hover:opacity-80"
+          />
         </div>
       </div>
     </div>
